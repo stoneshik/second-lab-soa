e36b04f11070bd5663db27a68d48a8f17d129cf5
stoneshik <strebilly@mail.ru>
2026-01-17 14:19:34 +0300

    добавил в trustore сертификат добавляемого сервиса

--- src/main/java/lab/soa/infrastructure/config/RestTemplateConfig.java
+++ src/main/java/lab/soa/infrastructure/config/RestTemplateConfig.java
@@ -1,21 +1,20 @@
 package lab.soa.infrastructure.config;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
+import java.security.KeyStore;
 import java.time.Duration;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
@@ -24,18 +23,15 @@ public class RestTemplateConfig {
     @Value("${proxy.target-service.url}")
     private String targetServiceUrl;
 
+    @Value("${server.ssl.trust-store:classpath:wildfly.p12}")
+    private Resource trustStore;
+
+    @Value("${server.ssl.trust-store-password:changeit}")
+    private String trustStorePassword;
+
     @Bean
-    RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder)
-            throws NoSuchAlgorithmException, KeyManagementException {
-        SSLContext sslContext = SSLContext.getInstance("TLS");
-        sslContext.init(null,
-            new TrustManager[]{new X509TrustManager() {
-                public X509Certificate[] getAcceptedIssuers() { return null; }
-                public void checkClientTrusted(X509Certificate[] certs, String authType) { }
-                public void checkServerTrusted(X509Certificate[] certs, String authType) { }
-            }},
-            new java.security.SecureRandom()
-        );
+    RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) throws Exception {
+        SSLContext sslContext = createSSLContext();
         SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory() {
             @Override
             protected void prepareConnection(HttpURLConnection connection, String httpMethod)
@@ -49,10 +45,24 @@ public class RestTemplateConfig {
             }
         };
         return restTemplateBuilder
-            .rootUri(targetServiceUrl)
-            .requestFactory(() -> requestFactory)
-            .connectTimeout(Duration.ofSeconds(5))
-            .readTimeout(Duration.ofSeconds(30))
-            .build();
+                .rootUri(targetServiceUrl)
+                .requestFactory(() -> requestFactory)
+                .connectTimeout(Duration.ofSeconds(5))
+                .readTimeout(Duration.ofSeconds(30))
+                .build();
+    }
+
+    private SSLContext createSSLContext() throws Exception {
+        KeyStore trustStore = KeyStore.getInstance("PKCS12");
+        try (InputStream inputStream = this.trustStore.getInputStream()) {
+            trustStore.load(inputStream, trustStorePassword.toCharArray());
+        }
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
+            TrustManagerFactory.getDefaultAlgorithm()
+        );
+        trustManagerFactory.init(trustStore);
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustManagerFactory.getTrustManagers(), null);
+        return sslContext;
     }
 }
